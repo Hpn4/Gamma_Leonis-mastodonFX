94ff6e2ec9359ab122de31a91810a3463e26fa23
Etienne Senigout <etienne.senigout@epita.fr>
2023-03-19 20:10:04 +0100

    Update API and refactoring and start to create UI

---
+++ src/main/java/eus/ehu/gleonis/gleonismastodonfx/api/apistruct/Suggestion.java
@@ -0,0 +1,16 @@
+package eus.ehu.gleonis.gleonismastodonfx.api.apistruct;
+
+public class Suggestion {
+
+    String source;
+
+    Account account;
+
+    public String getSource() {
+        return source;
+    }
+
+    public Account getAccount() {
+        return account;
+    }
+}
