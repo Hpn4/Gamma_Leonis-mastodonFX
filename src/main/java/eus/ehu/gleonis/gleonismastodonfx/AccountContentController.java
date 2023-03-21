cabcc8de52dc15948ae16b7938191a41985db51c
EnderNight <ndxendernight@gmail.com>
2023-03-21 16:59:10 +0100

    Added main controller for sunday

---
+++ src/main/java/eus/ehu/gleonis/gleonismastodonfx/AccountContentController.java
@@ -0,0 +1,68 @@
+package eus.ehu.gleonis.gleonismastodonfx;
+
+import eus.ehu.gleonis.gleonismastodonfx.api.API;
+import eus.ehu.gleonis.gleonismastodonfx.api.apistruct.Account;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+
+public class AccountContentController {
+
+    private API api = new API();
+
+    private Account account;
+
+    @FXML
+    private Button followersButton;
+
+    @FXML
+    private Button followingButton;
+
+    @FXML
+    private ListView<Account> listView;
+
+    @FXML
+    private Button tootsButton;
+
+    @FXML
+    void onFollowersClick(ActionEvent event) {
+        showFollowers();
+    }
+
+    @FXML
+    void onFollowingsClick(ActionEvent event) {
+        // showFollowings();
+    }
+
+    @FXML
+    void onTootsClick(ActionEvent event) {
+        // showToots();
+    }
+
+    @FXML
+    void initialize() {
+
+        account = api.verifyCredentials();
+
+
+
+    }
+
+    public void showFollowers() {
+
+
+        ObservableList<Account> followers = FXCollections.observableList(api.getAccountFollowers(account.getId(), 20).getElement());
+
+        if (listView != null) {
+            listView.setItems(followers);
+            listView.setCellFactory(param -> {
+                    var cell = new AccountItemCell();
+                    return cell;
+            });
+        }
+    }
+
+}
--- src/main/java/eus/ehu/gleonis/gleonismastodonfx/HelloApplication.java
+++ src/main/java/eus/ehu/gleonis/gleonismastodonfx/HelloApplication.java
@@ -10,9 +10,9 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("account_content.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
-        stage.setTitle("Hello!");
+        stage.setTitle("MastodonFX Client");
         stage.setScene(scene);
         stage.show();
     }
