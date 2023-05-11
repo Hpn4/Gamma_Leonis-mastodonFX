89211ef3ac8e5e3f4d667bde63eab52f7f886b89
Etienne Senigout <etienne.senigout@epita.fr>
2023-05-11 16:40:14 +0200

    Compiling only windows

--- build_app_windows.bat
+++ build_app_windows.bat
@@ -72,7 +72,7 @@ call "%JAVA_HOME%\bin\jlink" ^
   --no-man-pages ^
   --compress=2 ^
   --strip-debug ^
-  --module-path target/installer/input/libs/
+  --module-path target/installer/input/libs/ ^
   --add-modules %detected_modules%%manual_modules% ^
   --include-locales=en,de ^
   --output target/java-runtime
